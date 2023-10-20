9abfa617d7db6703507f0c14354fef793159a4d9
oceliazhang <oceliaaa@163.com>
2023-10-19 20:16:06 -0700

    add css

--- src/Kanbas/Courses/Assignments/index.js
+++ src/Kanbas/Courses/Assignments/index.js
@@ -3,6 +3,9 @@ import { Link, useParams } from "react-router-dom";
 import db from "../../Database";
 import Button from "./button";
 import './index.css';
+import { BsFillPencilFill, BsJournalMedical, BsThreeDotsVertical } from "react-icons/bs";
+import "./index.css";
+import { AiFillCheckCircle, AiOutlinePlus } from "react-icons/ai";
 
 function Assignments() {
   const { courseId } = useParams();
@@ -14,16 +17,65 @@ function Assignments() {
       {/* <h2>Assignments for course {courseId}</h2> */}
       <Button />
       <div className="list-group">
-        {courseAssignments.map((assignment) => (
-          <Link
-            key={assignment._id}
-            to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}
-            className="list-group-item">
-            {assignment.title}
-          </Link>
-        ))}
+        <ul class="list-group border-start border-5 border-success ">
+          <div className="d-flex justify-content-between">
+            <BsThreeDotsVertical className="gray-icon" />
+            <h3 className="col-8">Assignments</h3>
+            <button className="no-radio">40% of Total</button>
+            <AiOutlinePlus className="gray-icon" />
+            <BsThreeDotsVertical className="gray-icon" />
+          </div>
+          <div className="list-group">
+            {courseAssignments.map((assignment) => (
+              <Link
+                key={assignment._id}
+                to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}
+                className="list-group-item">
+                <div className="d-flex justify-content-between list-group-item-secondary">
+                  <BsFillPencilFill className="green-icon" />
+                  <h3 className="col-8">{assignment.title}</h3>
+                  <AiFillCheckCircle className="green-icon" />
+                  <BsThreeDotsVertical className="gray-icon" />
+                </div>
+
+              </Link>
+            ))}
+          </div>
+        </ul>
       </div>
+
     </div>
   );
 }
 export default Assignments;
+
+
+// import React from "react";
+// import { Link, useParams } from "react-router-dom";
+// import db from "../../Database";
+// import Button from "./button";
+// import './index.css';
+
+// function Assignments() {
+//   const { courseId } = useParams();
+//   const assignments = db.assignments;
+//   const courseAssignments = assignments.filter(
+//     (assignment) => assignment.course === courseId);
+//   return (
+//     <div className="away-from-edge">
+//       {/* <h2>Assignments for course {courseId}</h2> */}
+//       <Button />
+//       <div className="list-group">
+//         {courseAssignments.map((assignment) => (
+//           <Link
+//             key={assignment._id}
+//             to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}
+//             className="list-group-item">
+//             {assignment.title}
+//           </Link>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+// export default Assignments;
--- src/Kanbas/Courses/Modules/ModuleList.js
+++ src/Kanbas/Courses/Modules/ModuleList.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import db from "../../Database";
+import { BsJournalMedical, BsThreeDotsVertical } from "react-icons/bs";
+import "./index.css";
+import { AiFillCheckCircle, AiOutlinePlus } from "react-icons/ai";
+
 
 function ModuleList() {
   const { courseId } = useParams();
@@ -9,29 +13,73 @@ function ModuleList() {
     <ul className="list-group">
       {
         modules
-         .filter((module) => module.course === courseId)
-         .map((module, index) => (
-           <li key={index} className="list-group-item">
-             <h3>{module.name}</h3>
-             <p>{module.description}</p>
-             {
+          .filter((module) => module.course === courseId)
+          .map((module, index) => (
+            <li key={index} className="list-group-item border-start">
+              <div className="d-flex justify-content-between">
+                <BsJournalMedical className="green-icon"/>
+                <h3 className="col-10">{module.name}</h3>
+                <AiFillCheckCircle className="green-icon"/>
+                <AiOutlinePlus className="gray-icon"/>
+                <BsThreeDotsVertical className="gray-icon"/>
+              </div>
+              <p>{module.description}</p>
+              {
                 module.lessons && (
-                    <ul className="list-group">
-                        {
-                            module.lessons.map((lesson, index) => (
-                                <li key={index} className="list-group-item">
-                                    <h4>{lesson.name}</h4>
-                                    <p>{lesson.description}</p>
-                                </li>
-                            ))
-                        }
-                    </ul>
+                  <ul className="list-group list-group-item-secondary me-1">
+                    {
+                      module.lessons.map((lesson, index) => (
+                        <li key={index} className="list-group-item">
+                          <h4>{lesson.name}</h4>
+                          <p>{lesson.description}</p>
+                        </li>
+                      ))
+                    }
+                  </ul>
                 )
-             }
-           </li>
-      ))
+              }
+            </li>
+          ))
       }
     </ul>
   );
 }
 export default ModuleList;
+
+// import React from "react";
+// import { useParams } from "react-router-dom";
+// import db from "../../Database";
+
+// function ModuleList() {
+//   const { courseId } = useParams();
+//   const modules = db.modules;
+//   return (
+//     <ul className="list-group">
+//       {
+//         modules
+//          .filter((module) => module.course === courseId)
+//          .map((module, index) => (
+//            <li key={index} className="list-group-item">
+//              <h3>{module.name}</h3>
+//              <p>{module.description}</p>
+//              {
+//                 module.lessons && (
+//                     <ul className="list-group">
+//                         {
+//                             module.lessons.map((lesson, index) => (
+//                                 <li key={index} className="list-group-item">
+//                                     <h4>{lesson.name}</h4>
+//                                     <p>{lesson.description}</p>
+//                                 </li>
+//                             ))
+//                         }
+//                     </ul>
+//                 )
+//              }
+//            </li>
+//       ))
+//       }
+//     </ul>
+//   );
+// }
+// export default ModuleList;
