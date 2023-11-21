6d13b47f361060a2e8e6729cc1f24c533c0466c0
oceliazhang <oceliaaa@163.com>
2023-11-20 20:51:42 -0800

    change lab5 url first try

--- src/Labs/a5/index.js
+++ src/Labs/a5/index.js
@@ -1,41 +1,48 @@
+// import EncodingParametersInURLs from "./EncodingParametersInURLs.js";
+// import WorkingWithObjects from "./WorkingWithObjects.js";
+// import WorkingWithArrays from "./WorkingWithArrays.js";
+
+
+// function Assignment5() {
+//     return (
+//       <div>
+//         <h1>Assignment 5</h1>
+//         <div className="list-group">
+//           <a href="http://localhost:4000/a5/welcome"
+//              className="list-group-item">
+//             Welcome
+//           </a>
+//           <EncodingParametersInURLs />
+//           <WorkingWithObjects/>
+//           <WorkingWithArrays/>
+//         </div>
+//       </div>
+//     );
+//   }
+//   export default Assignment5;
+
+import React from 'react';
 import EncodingParametersInURLs from "./EncodingParametersInURLs.js";
 import WorkingWithObjects from "./WorkingWithObjects.js";
 import WorkingWithArrays from "./WorkingWithArrays.js";
 
-
 function Assignment5() {
+    const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:4000";
+    const welcomeURL = `${API_BASE}/a5/welcome`;
+
     return (
       <div>
         <h1>Assignment 5</h1>
         <div className="list-group">
-          <a href="http://localhost:4000/a5/welcome"
-             className="list-group-item">
+          <a href={welcomeURL} className="list-group-item">
             Welcome
           </a>
           <EncodingParametersInURLs />
-          <WorkingWithObjects/>
-          <WorkingWithArrays/>
+          <WorkingWithObjects />
+          <WorkingWithArrays />
         </div>
       </div>
     );
-  }
-  export default Assignment5;
+}
 
-
-/*
-function Assignment5() {
-    return (
-      <div>
-        <h1>Assignment 5</h1>
-        <div className="list-group">
-          <a href="http://localhost:4000/a5/welcome"
-             className="list-group-item">
-            Welcome
-          </a>
-        </div>
-        <SimpleAPIExamples />
-      </div>
-    );
-  }
-  export default Assignment5;
-*/
+export default Assignment5;
