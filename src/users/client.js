cbfb434448b89731628329201e3125358dfdeee3
oceliazhang <oceliaaa@163.com>
2023-12-03 23:35:19 -0800

    npm install agian

--- src/users/client.js
+++ src/users/client.js
@@ -40,6 +40,7 @@ export const signup = async (user) => {
   const response = await request.post(`${USERS_API}/signup`, user);
   return response.data;
 };
+
 export const signout = async () => {
   const response = await request.post(`${USERS_API}/signout`);
   return response.data;
